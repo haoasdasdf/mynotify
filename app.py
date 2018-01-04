bdf8871657cd29be4f23bd2fcd3f6348d7d9e50d
kkaku <kkaku@bemap.co.jp>
2018-01-04 12:25:56 +0900

    return host

--- app.py
+++ app.py
@@ -9,4 +9,4 @@ def hello_world():
     # return "This is a test page"  # ここで返す内容は何でもよい
     return MAILGUN_API_KEY
 
-run(host="127.0.0.1", port=int(os.environ.get("PORT", 5000)))
+run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
