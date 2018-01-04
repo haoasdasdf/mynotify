cf42c2899c57dbe944abd91dd10ef8638c099efc
kkaku <kkaku@bemap.co.jp>
2018-01-04 13:24:27 +0900

    fix bug

--- send.py
+++ send.py
@@ -1,6 +1,5 @@
 import os
 import sendgrid
-import smtplib
 import time
 from threading import Timer
 from sendgrid.helpers.mail import *
@@ -14,26 +13,23 @@ TO_ADDRESS = os.environ.get('TO_ADDRESS')
 FROM_ADDRESS = os.environ.get('SENDGRID_USERNAME')
 
 
-class Email:
-    def send_mail(self, to_address, from_address, subject, plaintext):
-        sg = sendgrid.SendGridAPIClient(
-            apikey=SENDGRID_API)
+class SendEmail:
+    def send_mail(self, subject, plaintext):
+        sg = sendgrid.SendGridAPIClient(apikey=SENDGRID_API)
         from_email = Email(FROM_ADDRESS)
         to_email = Email(TO_ADDRESS)
+        content = Content("text/plain", plaintext)
         mail = Mail(from_email, subject, to_email, content)
         response = sg.client.mail.send.post(request_body=mail.get())
-        print(response.status_code)
-        print(response.body)
-        print(response.headers)
+        return (response)
 
 
 def run():
-    # t = Timer(3600, run)
-    # t.start()
-    a = Email()
-    res = a.send_mail(TO_ADDRESS, FROM_ADDRESS,
-                      "testEmail", "can you receive?")
-    print(res.status_code)
+    a = SendEmail()
+    r = a.send_mail("testEmail", "can you receive?")
+    print(r.status_code)
+    print(r.body)
+    print(r.headers)
 
 
 if __name__ == '__main__':
