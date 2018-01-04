48112f14fe9fa3839329c0e747c93e35b03b91fa
kkaku <kkaku@bemap.co.jp>
2018-01-04 16:18:08 +0900

    change alot

--- app.py
+++ app.py
@@ -2,11 +2,27 @@
 
 import os
 from bottle import route, run
+from threading import Timer
+import requests
+
+APP_URL = os.environ.get('APP_URL')
+
 
 @route("/")
 def hello_world():
-    MAILGUN_API_KEY=os.environ.get('SENDGRID_USERNAME')
-    # return "This is a test page"  # ここで返す内容は何でもよい
-    return MAILGUN_API_KEY
+    return "This is a test page"  # ここで返す内容は何でもよい
 
-run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
+def call():
+    t = Timer(1799, call)
+    t.start()
+    r = requests.get(APP_URL)
+
+
+def main():
+    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    call()
+
+
+if __name__ == '__main__':
+    main()
---
+++ notify.py
@@ -0,0 +1,50 @@
+import os
+
+import requests
+import sendgrid
+from bs4 import BeautifulSoup
+from sendgrid.helpers.mail import *
+
+TARGET_URL = os.environ.get('TARGET_URL')
+SENDGRID_API = os.environ.get('SENDGRID_API')
+SENDGRID_USERNAME = os.environ.get('SENDGRID_USERNAME')
+SENDGRID_PASSWORD = os.environ.get('SENDGRID_PASSWORD')
+TO_ADDRESS = os.environ.get('TO_ADDRESS')
+FROM_ADDRESS = os.environ.get('SENDGRID_USERNAME')
+MAIL_SUBJECT = os.environ.get('MAIL_SUBJECT')
+MAIL_CONTENT = os.environ.get('MAIL_CONTENT')
+
+
+class NotifyApp:
+
+    def __init__(self):
+        self.search = self._search()
+        print(self.search)
+        if(self.search is True):
+            self._sendMail()
+
+    def _search(self):
+        r = requests.get(TARGET_URL)
+        soup = BeautifulSoup(r.text, 'lxml')
+        sub = soup.body.find_all('a', class_='submit')
+        if(sub):
+            return True
+        return False
+
+    def _sendMail(self):
+        sg = sendgrid.SendGridAPIClient(apikey=SENDGRID_API)
+        from_email = Email(FROM_ADDRESS)
+        to_email = Email(TO_ADDRESS)
+        content = Content("text/plain", MAIL_CONTENT)
+        mail = Mail(from_email, MAIL_SUBJECT,
+                    to_email, content)
+        response = sg.client.mail.send.post(request_body=mail.get())
+        return (response)
+
+
+def main():
+    app = NotifyApp()
+
+
+if __name__ == '__main__':
+    main()
