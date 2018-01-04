fb1f3bc353698abeced5e2d48307e84ca08be6ca
kkaku <kkaku@bemap.co.jp>
2018-01-04 12:22:02 +0900

    change host

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from bottle import route, run
 
 @route("/")
 def hello_world():
-    return "This is a test page"  # ここで返す内容は何でもよい
+    MAILGUN_API_KEY=os.environ.get('MAILGUN_API_KEY')
+    # return "This is a test page"  # ここで返す内容は何でもよい
+    return MAILGUN_API_KEY
 
-
-run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+run(host="127.0.0.1", port=int(os.environ.get("PORT", 5000)))
