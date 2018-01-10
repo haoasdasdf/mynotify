e3de70c122f50aa5da6dbfb1bc96ae072f1aaa2a
kkaku <kkaku@bemap.co.jp>
2018-01-10 18:34:32 +0900

    add get price

--- price.py
+++ price.py
@@ -5,8 +5,7 @@ from urllib import parse
 import MySQLdb
 import os
 
-# CLEARDB_DATABASE_URL = os.environ.get('CLEARDB_DATABASE_URL')
-CLEARDB_DATABASE_URL = 'mysql://b8f52406326a20:c044b4d5@us-cdbr-iron-east-05.cleardb.net/heroku_d72f255429a03f3?reconnect=true'
+CLEARDB_DATABASE_URL = os.environ.get('CLEARDB_DATABASE_URL')
 url = parse.urlparse(CLEARDB_DATABASE_URL)
 db_info = {
     'NAME': url.path[1:],
@@ -21,6 +20,7 @@ class Price:
     def __init__(self):
         self._get_price()
         self._update_price()
+        # self._watch()
 
     def _get_price(self):
         base_url = 'https://www.coingecko.com/ja/%E7%9B%B8%E5%A0%B4%E3%83%81%E3%83%A3%E3%83%BC%E3%83%88/'
@@ -46,7 +46,7 @@ class Price:
                               host=db_info.get('HOST'),
                               db=db_info.get('NAME'))
         cur = con.cursor()
-        update_sql = "update price set 'price'='{price}' and 'update_date'='{time}' where 'symbol'='{symbol}';"
+        update_sql = "update price set price='{price}' , update_date='{time}' where symbol='{symbol}';"
         now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         cur.execute(update_sql.format(price=self.btc, time=now, symbol="btc",))
         cur.execute(update_sql.format(price=self.zny, time=now, symbol="zny",))
@@ -54,32 +54,26 @@ class Price:
         cur.execute(update_sql.format(
             price=self.doge, time=now, symbol="doge",))
         cur.execute(update_sql.format(price=self.xp, time=now, symbol="xp",))
-        test=update_sql.format(price="1000$", time=now, symbol="btc",)
-        print(test)
+        con.commit()
         cur.close
         con.close
 
     def _watch(self):
+        now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        update_sql = "update price set price='{price}' , update_date='{time}' where symbol='{symbol}'"
+        test = update_sql.format(price="950$", time=now, symbol="btc")
+        print(test)
         con = MySQLdb.connect(user=db_info.get('USER'),
                               passwd=db_info.get('PASSWORD'),
                               host=db_info.get('HOST'),
                               db=db_info.get('NAME'))
+
         cur = con.cursor()
-        sql = "select * from price"
-        cur.execute(sql)
-        rows = cur.fetchall()
+        cur.execute(str(update_sql.format(
+            price="950$", time=now, symbol="btc")))
+        con.commit()
         cur.close
         con.close
-        info = []
-        for row in rows:
-            one = {}
-            one.update({
-                'name': row[1],
-                'price': row[3],
-                'update_time': row[4]
-            })
-            info.append(one)
-        print(info)
 
 
 def main():
